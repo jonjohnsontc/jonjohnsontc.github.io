763ab681f42b858010c2041dfb6894e5860cc80d
Jon Johnson <jonjohnsontc@live.com>
2021-08-01 18:54:39 -0700

    have a slightly more creatively fullfilling blog template

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,18 +7,52 @@ function BlogListItem(props) {
     <div className={`blog-list-box block`}>
       <ul className={`level`}>
         <li className={`is-size-3`}>{props.date}</li>
-        <li className={`is-size-3 level-item`}><Link to="/blog/test-post/">{props.title}</Link></li>
+        <li className={`is-size-3 level-item`}>
+          <Link to={props.link}>{props.title}</Link>
+        </li>
       </ul>
     </div>
   );
 }
 
-export default function Blog() {
-  // 
-  // const blogs = graphql`Query`
+export const blogQuery = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          slug
+          date
+        }
+      }
+    }
+  }
+`;
+
+export default function Blog({ data }) {
+  // TODO: I want to grab all post titles, slugs, and dates in src/markdown-pages
+  const { allMarkdownRemark } = data;
+  const { nodes } = allMarkdownRemark;
+
+  let posts = [];
+
+  for (let index = 0; index < nodes.length; index++) {
+    const post = nodes[index];
+    posts.push(
+      <BlogListItem
+        title={post.frontmatter.title}
+        date={post.frontmatter.date}
+        link={post.frontmatter.slug}
+      />
+    );
+  }
+
+  const blogLog = console.log(posts.length);
+
   return (
     <div className={`container blog`}>
-      <BlogListItem date="5/12/88" title="Little Jonny was born" />
+      <div>Total posts {nodes.length}</div>
+      {posts}
     </div>
   );
 }
--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,22 +1,33 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { markdownRemark } = data; 
+  const { frontmatter, html } = markdownRemark;
+
+  const title = frontmatter.title.split(" ");
+
+  console.log(title);
+
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        {/* <h1>{frontmatter.title}</h1> */}
-        {/* <h2>{frontmatter.date}</h2> */}
-        <div
-          className={`blog-post-content container`}
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <div className={`blog-post-container container`}>
+      <div className={`blog-post hero is-medium has-background-grey-darker`}>
+        {/* We render a blank hero-body, and hero-foot with text to 
+        give the title at bottom look */}
+        <div className={`hero-body`}></div>
+        <div className={`hero-foot`}>
+          <p className={`title is-size-1 is-inline has-text-white`}>{frontmatter.title}</p>
+          <p className={`pl-6 subtitle is-inline has-text-white`}>{frontmatter.date}</p>
+        </div>
       </div>
+
+      <div
+        className={`blog-post-content mt-6`}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </div>
-  )
+  );
 }
 export const pageQuery = graphql`
   query($id: String!) {
@@ -29,4 +40,4 @@ export const pageQuery = graphql`
       }
     }
   }
-  `
+`;
