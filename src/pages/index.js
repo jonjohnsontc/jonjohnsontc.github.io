60664459e68af4b8cbce74a01e001d15e8661ca0
Jon Johnson <jonjohnsontc@live.com>
2021-09-05 11:20:22 -0700

    added page titles, and actually put my name down in the about section for SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,35 @@
-import React from "react"
-import { Splash } from "../components/page"
-import Logo from "../components/logo"
+import React from "react";
+import { Splash } from "../components/page";
+import Logo from "../components/logo";
+import { Helmet } from "react-helmet";
 
 function Greeting(props) {
-  let greeting = props.greeting || "Hi, I'm Jon :0)"
+  let greeting = props.greeting || "Hi, I'm Jon :0)";
 
-  return <p className={`title is-family has-text-centered`}>{greeting}</p>
+  return <p className={`title is-family has-text-centered`}>{greeting}</p>;
 }
 
 function Bio() {
   return (
     <p className={`subtitle has-text-centered`}>
-      I'm a Software Engineer and creator of a few random things. I also love to write about
-      what I'm working on from time to time. Hope you enjoy checking things out.
+      I'm a Software Engineer and creator of a few random things. I also love to
+      write about what I'm working on from time to time. Hope you enjoy checking
+      things out.
     </p>
-  )
+  );
 }
 
 export default function Home() {
-  return <Splash logo={<Logo width="300" height="300"/>} belowHeadline={<Bio />} prompt={<Greeting />} />
+  return (
+    <>
+      <Helmet>
+        <title>Welcome to my website ~ jonj.io</title>
+      </Helmet>
+      <Splash
+        logo={<Logo width="300" height="300" />}
+        belowHeadline={<Bio />}
+        prompt={<Greeting />}
+      />
+    </>
+  );
 }
